3ebab07786084cdbbb87cb5f50a1c6f3e11190ca
yupi <grafik@10g.pl>
2017-04-11 12:48:18 +0200

    jeszcze nie wyświetla zbioru

---
+++ src/Lista.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Helmut on 2017-04-11.
+ */
+public class Lista {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+
+        int numbers;
+        do {
+            System.out.println("podaj cyfrę dodatnią, Koniec jeśli podasz ujemną");
+            numbers = scan.nextInt();
+            if (numbers < 0) {
+                System.out.println("STOP");
+            } else
+                list.add(numbers);
+
+        }while ( numbers>=0);
+
+        System.out.println(list.);
+    }
+}
